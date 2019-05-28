6206c3a428126bbe7fc429d8cc95b1a806041b28
Konrad Plazinski <kplazinski@ecovadis.com>
2019-05-28 09:44:14 +0200

    source code for episodes 7 and 8

---
+++ Episodes7-8/Api/Controllers/ProjectsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectsController()
+        {
+            _projectService = ServiceProxy.Create<IProjectService>(new Uri("fabric:/KaasDemo/ProjectService"), new ServicePartitionKey(922337203685477580));
+        }
+
+        public async Task<string> Get()
+        {
+            return await _projectService.GetProductName();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<string> GetById(int id)
+        {
+            return await _projectService.GetProductNameById(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task AddNewProject(int id, [FromBody]string name)
+        {
+            await _projectService.AddProduct(id, name);
+        }
+    }
+}
---
+++ Episodes7-8/Contracts/IProjectService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace Contracts
+{
+    public interface IProjectService : IService
+    {
+        Task<string> GetProductName();
+
+        Task<string> GetProductNameById(int id);
+
+        Task AddProduct(int id, string name);
+    }
+}
